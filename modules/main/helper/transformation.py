08c13edaaf147911ae2a32b3a76bbeb74d512bce
trungpq <trungpq.ml@gmail.com>
2020-07-31 19:48:04 +0800

    Adding transformation

--- modules/main/helper/transformation.py
+++ modules/main/helper/transformation.py
@@ -1,2 +1,44 @@
 def get_transformation(X, Y, compute_optimal_scale=False):
-    pass
+    muX = np.mean(X, 0)
+    muY = np.mean(Y, 0)
+
+    X0 = X - muX
+    Y0 = Y - muY
+
+    ssX = (X0**2).sum()
+    ssY = (Y0**2).sum()
+
+    # centered Frobenius norm
+    normX = np.sqrt(ssX)
+    normY = np.sqrt(ssY)
+
+    # scale to equal (unit) norm
+    X0 = X0/normX
+    Y0 = Y0/normY
+
+    # optimum rotation matrix of Y
+    A = np.dot(X0.T, Y0)
+    U, s, Vt = np.linalg.svd(A, full_matrices=False)
+    V = Vt.T
+    T = np.dot(V, U.T)
+
+    # make sure we have a rotation
+    detT = np.linalg.det(T)
+    V[:, -1] *= np.sign(detT)
+    s[-1] *= np.sign(detT)
+    T = np.dot(V, U.T)
+
+    traceTA = s.sum()
+
+    if compute_optimal_scale:
+        b = traceTA * normX / normY
+        d = 1 - traceTA ** 2
+        Z = normX * traceTA*np.dot(Y0, T) + muX
+    else:
+        b = 1
+        d = 1 + ssY/ssX - 2 * traceTA * normY/normX
+        X = normY * np.dot(Y0, T) + muX
+
+    c = muX - b * np.dot(muY, T)
+
+    return d, Z, T, b, c
